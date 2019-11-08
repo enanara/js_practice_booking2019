858b16b9b2d2a9697ffd0acf2bb041993ec7f406
Enkhnaran <enaa.mongolia@gmail.com>
2019-11-08 19:45:28 +0000

    half complete

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,39 +1,53 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  var firstChar = word[0];
+  var cap = firstChar.toUpperCase();
+  var remain = word.slice(1, word.length);
+  return cap+remain;
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  var fnInitial = firstName[0].toUpperCase();
+  var lnInitial = lastName[0].toUpperCase();
+  return fnInitial + "." + lnInitial;
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  var vat = originalPrice*vatRate/100;
+  return Math.round((originalPrice+vat)*100)/100;
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  var sale = originalPrice*reduction/100;
+  return Math.round((originalPrice-sale)*100)/100;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  var x = str.length;
+  if (x%2===1){
+    return str.slice((x-1)/2,(x+1)/2);
+  } else {
+    return str.slice((x/2-1),(x/2+1));
+  }
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word.split("").reverse().join("")
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
+  return words.map(function(word){
+    return word.split("").reverse().join("")
+  });
 }
 
 function countLinuxUsers(users) {
