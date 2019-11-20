6d3d1f49a36ed1ba46af3cfd7bc6daf1d82685e5
Enkhnaran <enaa.mongolia@gmail.com>
2019-11-20 18:32:36 +0000

    wk4 findSmallNums for loop done

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,6 +1,12 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let result =[];
+  for (let i=0; i<nums.length;i++){
+    if (nums[i]<1){
+      result.push(nums[i]);
+    }
+  }
+  return result;
 }
 
 function findNamesBeginningWith(names, char) {
