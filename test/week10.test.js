445fc8e78aae607e373349c4dabce7004c13d798
Enkhnaran <enaa.mongolia@gmail.com>
2020-01-21 13:40:10 +0000

    week10 getScreentimeAlertList test created

--- test/week10.test.js
+++ test/week10.test.js
@@ -36,10 +36,139 @@ describe("createRange", () => {
     });
 });
 
+
 describe("getScreentimeAlertList", () => {
-    test("returns sum of numbers that are multiple of 3 or 5", () => {
-      expect(getScreentimeAlertList([5, 3, 7, 8, 1, 10])).toEqual([]);
+    const users = [
+        {
+            username: "beth_1234",
+            name: "Beth Smith",
+            screenTime: [
+                {
+                    date: "2020-01-21",
+                    usage: {
+                        twitter: 34,
+                        instagram: 20, 
+                        facebook: 40
+                    }
+                },
+                {
+                    date: "2020-01-22",
+                    usage: {
+                        twitter: 56,
+                        instagram: 40, 
+                        facebook: 31
+                    }
+                },
+                {
+                    date: "2020-01-23",
+                    usage: {
+                        twitter: 33,
+                        instagram: 60, 
+                        facebook: 35
+                    }
+                },
+                {
+                    date: "2020-01-24",
+                    usage: {
+                        twitter: 50,
+                        instagram: 50, 
+                        facebook: 50
+                    }
+                }
+            ]
+        },
+        {
+            username: "sam_j_1989",
+            name: "Sam Jones",
+            screenTime: [
+                {
+                    date: "2020-01-21",
+                    usage: {
+                        mapMyRun: 55,
+                        whatsApp: 50, 
+                        facebook: 10
+                    }
+                },
+                {
+                    date: "2020-01-22",
+                    usage: {
+                        mapMyRun: 2,
+                        instagram: 0, 
+                        facebook: 10,
+                        safari: 20
+                    }
+                },
+                {
+                    date: "2020-01-23",
+                    usage: {
+                        twitter: 0,
+                        mapMyRun: 2, 
+                        facebook: 15,
+                        safari: 10
+                    }
+                },
+                {
+                    date: "2020-01-24",
+                    usage: {
+                        twitter: 0,
+                        instagram: 0, 
+                        facebook: 0
+                    }
+                }
+            ]
+        },
+        {
+            username: "ena_0901",
+            name: "Ena Nara",
+            screenTime: [
+                {
+                    date: "2020-01-21",
+                    usage: {
+                        slack: 10,
+                        instagram: 10, 
+                        facebook: 10,
+                        gmail:10
+                    }
+                },
+                {
+                    date: "2020-01-22",
+                    usage: {
+                        slack: 20,
+                        instagram: 20, 
+                        facebook: 20,
+                        gmail:20
+                    }
+                },
+                {
+                    date: "2020-01-23",
+                    usage: {
+                        slack: 30,
+                        instagram: 30, 
+                        facebook: 30,
+                        gmail:30
+                    }
+                },
+                {
+                    date: "2020-01-24",
+                    usage: {
+                        slack: 40,
+                        instagram: 40, 
+                        facebook: 40,
+                        gmail:40
+                    }
+                }
+            ]
+        }
+    ];
+    test("returns array with usernames that has more thant 100 minutes of screentime on given date", () => {
+      expect(getScreentimeAlertList(users, "2020-01-21")).toEqual(["sam_j_1989"]);
+      expect(getScreentimeAlertList(users, "2020-01-22")).toEqual(["beth_1234"]);
+      expect(getScreentimeAlertList(users, "2020-01-23")).toEqual(["beth_1234", "ena_0901"]);
+      expect(getScreentimeAlertList(users, "2020-01-24")).toEqual(["beth_1234", "ena_0901"]);
     });
+    test("if given date data is not reacheble return message", () => {
+        expect(getScreentimeAlertList(users, "2020-01-25")).toEqual("")
+    })
 });
 
 describe("hexToRGB", () => {
